2cad98548037d9da66fbc8d8f93f55cfc4803214
Duy <duypersonal512@gmail.com>
2025-12-28 07:41:02 -0600

    return token on auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,35 +1,75 @@
+import jwt from 'jsonwebtoken'
+import fs from 'fs'
 import { prisma } from "../lib/prisma.js"
 import { genPass, verifyPass } from "../lib/passwordUtils.js"
 
 const postLogin = async (req, res) => {
-    const {username, password} = req.body
+    try {
 
-    const user = await prisma.user.findFirst({
-        where: {
-            username: username
+        const {username, password} = req.body
+
+        const user = await prisma.user.findFirst({
+            where: {
+                username: username
+            }
+        })
+
+        if(!user){
+            res.send(400).send('user not found')
         }
-    })
-    console.log(user)
+        if (verifyPass(salt, hash, pass)){
+            const privateKey = fs.readFileSync('private.key');
+
+            jwt.sign({ userId: user.id }, privateKey, { algorithm: 'RS256' }, (err, token) => {
+                if(err){
+                    res.status(400).send('error')
+                }else {
+                    res.send(token)
+
+                }
+            });
+        } else {
+            res.status(400).send('wrong password')
+        }
+    
+
+    } catch (err){
+        res.status(500).send('unkown error');
+    }
 }
 
 const postSignup = async (req, res) => {
-    const {username, password} = req.body
+    try {
+        const {username, password} = req.body
 
-    const saltHash = genPass(password);
-    const salt = saltHash.salt;
-    const hash = saltHash.hash;
+        const saltHash = genPass(password);
+        const salt = saltHash.salt;
+        const hash = saltHash.hash;
 
-    const newUser = await prisma.user.create({
-        data: {
-            username: username,
-            salt: salt,
-            hash: hash,
-        }
-    })
+        const newUser = await prisma.user.create({
+            data: {
+                username: username,
+                salt: salt,
+                hash: hash,
+            }
+        })
+        
+        const privateKey = fs.readFileSync('private.key');
 
-    res.json(newUser);
+        jwt.sign({ userId: user.id }, privateKey, { algorithm: 'RS256' }, (err, token) => {
+            if(err){
+                res.status(400).send('error')
+            }else {
+                res.send(token)
+
+            }
+        });
+    }catch (err){
+        res.status(500).send('unkown error');
+    }
+    
+
 }
-
 
 export default {
     postLogin,
